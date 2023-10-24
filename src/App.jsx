ad1bc5675332866d953e71f5dfb3d11d857c675c
Issacboox <nurarat0024@gmail.com>
2023-10-24 22:15:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// import * as ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error"
+import Menu, { Loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder,{ Action as createOrderAction} from "./features/order/CreateOrder";
+import Order, { Loader as orderLoad} from "./features/order/Order";
+import AppLayout from "./ui/AppLayout"
+import { Action as updateOrderAction } from './features/order/UpdateOrder';
+
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error/>,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoad,
+        errorElement: <Error/>,
+        action: updateOrderAction,
+      },
+    ]
+  },
+ 
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
